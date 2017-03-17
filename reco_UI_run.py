9d0ee3c84ddd6505ce4d35ced4e061af34b6e20c
Sonali Agrawal <sonali9696@gmail.com>
2017-03-18 00:10:46 +0530

    Add files via upload

--- reco_UI_run.py
+++ reco_UI_run.py
@@ -2,12 +2,21 @@
 from recoUI import Ui_MainWindow
 import sys
 from PyQt4 import QtGui,QtCore
+import os
 
 class Panel(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self,parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        #self.ui.loginButton.clicked.connect(self.loginFunc)
+        self.ui.signupButton.clicked.connect(self.signUpFunc)
+
+    def loginFunc(self):
+        os.system('python login_UI_run.py')
+
+    def signUpFunc(self):
+        os.system('python signup_UI_run.py')
 
 def main():
     app=QtGui.QApplication(sys.argv) 
