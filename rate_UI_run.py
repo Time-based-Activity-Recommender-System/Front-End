1b0fc598fbabeb54ab8aa8e8b6068061784b07aa
Asmita Poddar <asmita_poddar@yahoo.co.in>
2017-03-18 05:54:34 -0700

    Add files via upload

---
+++ rate_UI_run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from rateUI import Ui_MainWindow
+import sys
+from PyQt4 import QtGui,QtCore
+
+class Rate(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QWidget.__init__(self,parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+	
+
+def main():
+    app=QtGui.QApplication(sys.argv) 
+    ui = Rate() 
+    ui.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
