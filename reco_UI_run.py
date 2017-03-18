5df9a4fc1b92deee82a18be71679e3d206c7171b
Sonali Agrawal <sonali9696@gmail.com>
2017-03-18 17:24:04 +0530

    Add files via upload

--- reco_UI_run.py
+++ reco_UI_run.py
@@ -4,7 +4,7 @@ import sys
 from PyQt4 import QtGui,QtCore
 import os
 
-class Panel(QtGui.QMainWindow):
+class FrontPage(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self,parent)
         self.ui = Ui_MainWindow()
@@ -20,7 +20,7 @@ class Panel(QtGui.QMainWindow):
 
 def main():
     app=QtGui.QApplication(sys.argv) 
-    ui = Panel() 
+    ui = FrontPage() 
     ui.show()
     sys.exit(app.exec_())
 
--- signup_UI_run.py
+++ signup_UI_run.py
@@ -1,18 +1,24 @@
-
 #!/usr/bin/env python
-from loginUI import Ui_MainWindow
+from signupUI import Ui_MainWindow
 import sys
 from PyQt4 import QtGui,QtCore
 
-class Login(QtGui.QMainWindow):
+
+class SignUp(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self,parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.credentials()
+
+    def credentials(self): 
+        self.name = self.ui.name_lineEdit
+        self.password = self.ui.password_lineEdit
+        #TODO: name,password check in database     
 
 def main():
     app=QtGui.QApplication(sys.argv) 
-    ui = Login() 
+    ui = SignUp() 
     ui.show()
     sys.exit(app.exec_())
 
