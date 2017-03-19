5efcfb6c60789d63426f452b729e3e4f7c7cbeaf
Sonali Agrawal <sonali9696@gmail.com>
2017-03-19 14:45:30 +0530

    Delete entertime.py

--- entertime.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'mainwindow.ui'
-#
-# Created: Sat Mar 18 23:32:53 2017
-#      by: PyQt4 UI code generator 4.10.4
-#
-# WARNING! All changes made in this file will be lost!
-
