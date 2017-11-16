fe6f8abe31161d4857c6f509418535adf8cd9ad4
Sonali Agrawal <sonali9696@gmail.com>
2017-11-17 00:26:44 +0530

    resolving bugs

--- finalPage3_UI_run.py
+++ finalPage3_UI_run.py
@@ -1,96 +1,5 @@
 #!/usr/bin/env python
-from finalPage3UI import Ui_MainWindow
-import sys
-from PyQt4 import QtGui,QtCore
-import os
-import pandas as pd
 
-class FinalPage3(QtGui.QMainWindow):
-    def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self,parent)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.initUI()
-        self.reco()
-        self.ui.pushButton.clicked.connect(self.home)
-        self.ui.pushButton_2.clicked.connect(self.logout)
-
-    def initUI(self):
-        self.setWindowTitle('Recommendations')
-        self.center()
-        self.show()
-
-    def center(self):
-        frameGm = self.frameGeometry()
-        centerPoint = QtGui.QDesktopWidget().availableGeometry().center()
-        frameGm.moveCenter(centerPoint)
-        self.move(frameGm.topLeft())
-
-    def reco(self):
-        #recommend songs
-        reco_cols = ['song_title']
-        reco_songs = pd.read_csv('song_reco.data', sep='\t', names=reco_cols, encoding='latin-1')
-        print reco_songs['song_title'][0]
-        #TODO: DONT DISPLAY ALL 15 ALWAYS. LOOK AT TIME
-        
-        k=0
-        for j in range(0,5):
-            for i in range(0,5):
-                self.ui.tableWidget.setItem(i, j, QtGui.QTableWidgetItem(reco_songs['song_title'][k]))
-                k = k + 1
-                if(k == 15):
-                    break
-            if(k == 15):
-                break
-
-        #recommend books 
-        #!/usr/bin/env python
-from finalPage3UI import Ui_MainWindow
-import sys
-from PyQt4 import QtGui,QtCore
-import os
-import pandas as pd
-
-class FinalPage3(QtGui.QMainWindow):
-    def __init__(self, parent=None):
-        QtGui.QWidget.__init__(self,parent)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-        self.initUI()
-        self.reco()
-        self.ui.pushButton.clicked.connect(self.home)
-        self.ui.pushButton_2.clicked.connect(self.logout)
-
-    def initUI(self):
-        self.setWindowTitle('Recommendations')
-        self.center()
-        self.show()
-
-    def center(self):
-        frameGm = self.frameGeometry()
-        centerPoint = QtGui.QDesktopWidget().availableGeometry().center()
-        frameGm.moveCenter(centerPoint)
-        self.move(frameGm.topLeft())
-
-    def reco(self):
-        #recommend songs
-        reco_cols = ['song_title']
-        reco_songs = pd.read_csv('song_reco.data', sep='\t', names=reco_cols, encoding='latin-1')
-        print reco_songs['song_title'][0]
-        #TODO: DONT DISPLAY ALL 15 ALWAYS. LOOK AT TIME
-        
-        k=0
-        for j in range(0,5):
-            for i in range(0,5):
-                self.ui.tableWidget.setItem(i, j, QtGui.QTableWidgetItem(reco_songs['song_title'][k]))
-                k = k + 1
-                if(k == 15):
-                    break
-            if(k == 15):
-                break
-
-        #recommend books 
-        #!/usr/bin/env python
 from finalPage3UI import Ui_MainWindow
 import sys
 from PyQt4 import QtGui,QtCore
@@ -142,12 +51,12 @@ class FinalPage3(QtGui.QMainWindow):
 	reco_books = pd.read_csv('book_reco.data', sep='\t', names=reco_bcols, encoding='latin-1')
         k=0
         for i in range(0,5):
-            self.ui.tableWidget_2.setItem(i, 0, QtGui.QTableWidgetItem(reco_books['book_title'][k+1]))
+            self.ui.tableWidget_2.setItem(i, 0, QtGui.QTableWidgetItem(reco_books['book_title'][k]))
             k = k + 1
             
 
 	for i in range(0,5):
-            self.ui.tableWidget_2.setItem(i, 1, QtGui.QTableWidgetItem(reco_books['book_title'][k+1]))
+            self.ui.tableWidget_2.setItem(i, 1, QtGui.QTableWidgetItem(reco_books['book_title'][k]))
             k = k + 1
 
         #recommend movies
@@ -157,7 +66,7 @@ class FinalPage3(QtGui.QMainWindow):
         
         k=0
         for i in range(0,5):
-            self.ui.tableWidget_3.setItem(i, 0, QtGui.QTableWidgetItem(reco_movies['movie_title'][k+1]))
+            self.ui.tableWidget_3.setItem(i, 0, QtGui.QTableWidgetItem(reco_movies['movie_title'][k]))
             k = k + 1
                 
     def home(self):
@@ -177,4 +86,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
